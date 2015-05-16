99970d8fc5f97ccb6751e99487e5ede73996123e
tspiegs work laptop <tspiegelman@amplify.com>
2015-05-16 12:08:30 -0400

    added ssh key functionality through the DO API

--- startup.sh
+++ startup.sh
@@ -6,3 +6,24 @@ echo "unzipping terrform!" \
 sudo unzip terraform_0.5.1_linux_amd64.zip -d /usr/bin/ 
 }
 
+
+echo "enter your DigitalOcean API Key to start setting up your new droplet"
+
+read DOKey
+
+echo $DOKey
+
+sshkeyFP=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+curlkeystatus=$(curl -X GET -H 'Content-Type: application/json' -H 'Authorization: Bearer 36357c32fc84612f6a0dc937b9c8da4ea49a51a8955c43debfeae29ff6ec392f'  "https://api.digitalocean.com/v2/account/keys" | grep -ci $sshkeyFP)
+if [ $curlkeystatus -eq 0 ]
+then
+  echo "Putting your SSH key in DO"
+  sshkeypub=$(cat ~/.ssh/id_rsa.pub)
+  apijson=$(echo "{\"name\":\"My SSH Public Key\",\"public_key\":\"$sshkeypub\"}")
+  curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $DO_PAT" -d "$apijson" "https://api.digitalocean.com/v2/account/keys"
+elif [ $curlkeystatus -eq 1 ]; then
+  echo "SSH Key properly in DO account, Continuing…."
+else
+  echo "can't detect ssh key status, something is VERY wrong here……"
+fi
+
