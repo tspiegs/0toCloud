308fb6adde5173ea25dd62c1c4821423b5676515
tspiegs work laptop <tspiegelman@amplify.com>
2015-05-16 18:16:27 -0400

    added metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             '0toCloud'
+maintainer       'tspiegs'
+license          'All rights reserved'
+description      'Installs/Configures your first cloud server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+version          '0.1.1'
+
