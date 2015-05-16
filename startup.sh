49d9a22a271f2392b10dfa3c38ff17e0a1ac089a
tspiegs work laptop <tspiegelman@amplify.com>
2015-05-16 17:09:50 -0400

    now creates a users rsa key pair if one does not already exist

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+#is terrform an executable application? 
 type terraform >/dev/null 2>&1 || { echo "Dowloading and setting up Terraform" \ 
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.5.1_linux_amd64.zip \ 
 echo "unzipping terrform!" \ 
@@ -7,12 +9,27 @@ sudo unzip terraform_0.5.1_linux_amd64.zip -d /usr/bin/
 }
 
 
+#checking if an rsa token exists @ ~/.ssh/id_rsa.pub and if not, creating one
+if [ -a ~/.ssh/id_rsa.pub ]; then
+  echo "public ssh key file exists, continuing"
+else
+  echo "~/.ssh/id_rsa.pub does not exists.  Would you like to create it? yes or no"
+  read creatersa
+  if [[ $creatersa == "yes" || $creatersa == "y" ]]; then
+    echo "creating rsa key pair"
+    ssh-keygen -t rsa
+  else
+    echo "rsa key pair exists, continuing"
+  fi
+fi
+
+
+#reading and storing DigitalOcean API Key
 echo "enter your DigitalOcean API Key to start setting up your new droplet"
 
 read DOKey
 
-echo $DOKey
-
+#checking if a the rsa public key stored in ~/.ssh/id_rsa.pub exists in provided DO account, and if not, POST it there
 sshkeyFP=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
 curlkeystatus=$(curl -X GET -H 'Content-Type: application/json' -H 'Authorization: Bearer 36357c32fc84612f6a0dc937b9c8da4ea49a51a8955c43debfeae29ff6ec392f'  "https://api.digitalocean.com/v2/account/keys" | grep -ci $sshkeyFP)
 if [ $curlkeystatus -eq 0 ]
@@ -26,4 +43,6 @@ elif [ $curlkeystatus -eq 1 ]; then
 else
   echo "can't detect ssh key status, something is VERY wrong here……"
 fi
+
+
 
